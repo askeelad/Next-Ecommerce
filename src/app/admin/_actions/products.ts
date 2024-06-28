61df7e39830490820cce128ab6942824348ec5fc
askeelad <khairulbashar19@gmail.com>
2024-06-28 16:48:58 +0600

    image path changed

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -28,12 +28,15 @@ export async function addProduct(previouState: unknown, formData: FormData) {
 
   const data = result.data;
 
-  await fs.mkdir(path.join(__dirname, "products"), { recursive: true });
-  const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
-  await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+  await fs.mkdir("public/products", { recursive: true });
+  const filePath = `/products/${crypto.randomUUID()}-${data.file.name}`;
+  await fs.writeFile(
+    `public${filePath}`,
+    Buffer.from(await data.file.arrayBuffer())
+  );
 
-  await fs.mkdir(path.join(__dirname, "/public/products"), { recursive: true });
-  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
+  await fs.mkdir("public/products", { recursive: true });
+  const imagePath = `/products/${crypto.randomUUID()}-${data.file.name}`;
   await fs.writeFile(
     `public${imagePath}`,
     Buffer.from(await data.image.arrayBuffer())
@@ -76,13 +79,10 @@ export async function updateProduct(
 
   let filePath = product.filePath;
   if (data.file != null && data.file.size > 0) {
-    await fs.unlink(product.filePath);
-    await fs.mkdir("/public/products", { recursive: true });
-    filePath = `products/${crypto.randomUUID()}-${data.file?.name}`;
-    await fs.writeFile(
-      `public${filePath}`,
-      Buffer.from(await data.file.arrayBuffer())
-    );
+    await fs.unlink(`public${product.filePath}`);
+    await fs.mkdir("public/products", { recursive: true });
+    filePath = `public/products/${crypto.randomUUID()}-${data.file?.name}`;
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
   }
 
   let imagePath = product.imagePath;
